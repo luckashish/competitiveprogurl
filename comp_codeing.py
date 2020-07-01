7651d26061d760810aee0f2860d6a06d82a9c9ee
ashish <luck.ashish@gmail.com>
2020-07-01 17:27:14 +0530

    Update comp_codeing.py
    fix some bug

--- comp_codeing.py
+++ comp_codeing.py
@@ -1,12 +1,23 @@
 import webbrowser
+import time
 
-webbrowser.open('https://leetcode.com/')
-webbrowser.open("https://www.hackerrank.com/")
-webbrowser.open("https://www.hackerearth.com/")
-webbrowser.open("https://a2oj.com/ladder?ID=4")
-webbrowser.open("https://codeforces.com/")
-webbrowser.open("https://github.com/")
-webbrowser.open("https://www.codechef.com/")
-webbrowser.open("https://www.geeksforgeeks.org/")
-webbrowser.open("https://cp-algorithms.com/")
-webbrowser.open("https://atcoder.jp/")
+try:
+    webbrowser.open_new_tab('https://leetcode.com')
+    time.sleep(2)   # added this to take first browser to open the other tabes otherwise it opens all tabes in new window
+    webbrowser.open_new_tab("https://www.hackerrank.com")
+    webbrowser.open_new_tab("https://www.hackerearth.com")
+    webbrowser.open_new_tab("https://a2oj.com/ladder?ID=4")
+    webbrowser.open_new_tab("https://codeforces.com")
+    webbrowser.open_new_tab("https://github.com")
+    webbrowser.open_new_tab("https://www.codechef.com")
+    webbrowser.open_new_tab("https://www.geeksforgeeks.org")
+    webbrowser.open_new_tab("https://cp-algorithms.com")
+    webbrowser.open_new_tab("https://atcoder.jp")
+except Exception:
+    print("some error happend")
+"""
+pat = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+webbrowser.register('chromex', None, webbrowser.BackgroundBrowser(pat))
+webbrowser.get('chromex').open_new_tab_new_tab("www.google.com")
+
+"""
