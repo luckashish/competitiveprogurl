4d16e987e7c63240c690edb505fca00bc8eee66e
ashish <luck.ashish@gmail.com>
2020-07-01 09:19:10 +0530

    Create comp_codeing.py
    simple way to be in touch with competitive programming

---
+++ comp_codeing.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+webbrowser.open('https://leetcode.com/')
+webbrowser.open("https://www.hackerrank.com/")
+webbrowser.open("https://www.hackerearth.com/")
+webbrowser.open("https://a2oj.com/ladder?ID=4")
+webbrowser.open("https://codeforces.com/")
+webbrowser.open("https://github.com/")
+webbrowser.open("https://www.codechef.com/")
+webbrowser.open("https://www.geeksforgeeks.org/")
+webbrowser.open("https://cp-algorithms.com/")
+webbrowser.open("https://atcoder.jp/")
